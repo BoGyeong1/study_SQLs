6a0db48ba9fe156b78bc49db9f5abded0764c4ac
HHNebula <sungsoomr@icloud.com>
2022-10-18 14:03:46 +0900

    방법 2 업데이트

--- SOO/w3schools/CategoryName_Products_Order.sql
+++ SOO/w3schools/CategoryName_Products_Order.sql
@@ -19,6 +19,24 @@ IN (
             )
 		)
 	)
-)    
+);
+
+SELECT DISTINCT OrderID 
+FROM OrderDetails
+WHERE ProductID 
+IN (
+    SELECT ProductID 
+    FROM Products
+    WHERE CategoryID 
+    IN (
+        SELECT CategoryID 
+        FROM Categories
+        WHERE CategoryName 
+        IN ('Dairy Products', 'Seafood')
+    ) AND (
+        Price BETWEEN 10 AND 50
+    );
+
+
 
 -- Number of Records: 379
