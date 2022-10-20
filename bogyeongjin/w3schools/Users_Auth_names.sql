990a069256acd748495823df2537d3df81a3ff5b
BoGyeong1 <wls6590@gmail.com>
2022-10-20 14:55:16 +0900

    UP users_Auths_names

---
+++ bogyeongjin/w3schools/Users_Auth_names.sql
@@ -0,0 +1,14 @@
+-- 4-1 이름, 이메일, 권한이름 표시
+SELECT users.NAME, users.EMAIL, auth_names.AUTH_NAME
+FROM (users INNER JOIN auths 
+	ON auths.UNIQUE_ID_USERS=users.UNIQUE_ID
+	AND users.NAME IN ('Teddy','Allen')) INNER JOIN auth_names
+		ON auth_names.UNIQUE_ID= auths.UNIQUE_ID_AUTH_NAMES
+    ;
+
+
+--     Allen	texas@imfblog.org	GUEST
+--     Allen	texas@imfblog.org	ADMIN
+--     Allen	texas@imfblog.org	MANAGER
+--     Teddy	norway@iotm.com	    GUEST
+-- 총 4개
